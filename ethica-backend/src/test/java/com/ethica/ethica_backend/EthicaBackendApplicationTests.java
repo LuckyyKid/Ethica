e0beed82c6ea22a437ae458b8eebdcb0e316e6cb
Bafing <bafingsimbokeita@gmail.com>
2025-05-15 02:57:37 -0400

    Add Spring Boot backend for Ethica V2

---
+++ ethica-backend/src/test/java/com/ethica/ethica_backend/EthicaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ethica.ethica_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EthicaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
